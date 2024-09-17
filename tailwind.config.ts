b8663d54341a0ecdab1a0045f1aebe8811201a11
Eric Tsai <eric492718@gmail.com>
2024-09-17 22:02:21 +0800

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -12,8 +13,34 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      keyframes: {
+        gifAnimation: {
+          from: { "background-image": "url(/img1.png)" },
+          to: { "background-image": "url(/img2.png)" },
+        },
+      },
+      animation: {
+        gifAnimation: "gifAnimation 0.15s linear infinite",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-3d"),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".animation-paused": {
+          "animation-play-state": "paused !important",
+        },
+        ".animation-running": {
+          "animation-play-state": "running !important",
+        },
+        ".leanback": {
+          transition: "1s",
+          transform: "perspective(20px) rotateX(2deg) translateY(-50px)",
+        },
+      });
+    }),
+  ],
 };
 export default config;
